9e401ae816fb3db1f9b670d9ab1b2d5366a3d207
vigreux-joel <vigreux.joel@hotmail.com>
2022-06-21 00:28:54 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import {NestFactory} from '@nestjs/core';
+import { AppModule } from './app.module';
+import {Logger} from "@nestjs/common";
+import 'dotenv/config';
+
+const port = process.env.PORT;
+
+
+async function bootstrap() {
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+  Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
+}
+bootstrap();
