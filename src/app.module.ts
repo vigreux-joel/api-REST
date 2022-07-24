7c19588e565dfe8fb46a78a0c5e5e31e6b693e40
vigreux-joel <vigreux.joel@hotmail.com>
2022-07-24 14:26:19 +0200

    refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,13 @@ import {ConfigModule} from "@nestjs/config";
 import {MongooseModule} from "@nestjs/mongoose";
 import {AppService} from "./app.service";
 import {Module} from "@nestjs/common";
-import {MongooseSchemasModule} from "./modules/mongoose-schemas/mongoose-schemas-module";
 import {UserModule} from "./modules/user/user.module";
 import {AuthModule} from "./modules/auth/auth.module";
-import {BlogModule} from "./modules/blog/blog.module";
 
 @Module({
     imports: [
         ConfigModule.forRoot(),
         MongooseModule.forRoot(process.env.MONGO_URL),
-        BlogModule,
-        MongooseSchemasModule,
         UserModule,
         AuthModule,
     ],
