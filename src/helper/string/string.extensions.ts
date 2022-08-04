e698962631369b7ddb0041517072b14ecce5f748
vigreux-joel <vigreux.joel@hotmail.com>
2022-08-04 23:42:14 +0200

    doc string.ucfirst

--- src/helper/string/string.extensions.ts
+++ src/helper/string/string.extensions.ts
@@ -1,11 +1,12 @@
 interface String {
+    /**
+     * Returns a string with the first character of string capitalized, if that character is alphabetic.
+     * @return {string}
+     **/
     ucfirst(): string;
 }
 
-/**
- * Returns a string with the first character of string capitalized, if that character is alphabetic.
- * @return {string}
- **/
+
 String.prototype.ucfirst = function (): string {
     return this.charAt(0).toUpperCase() + this.slice(1)
 };
